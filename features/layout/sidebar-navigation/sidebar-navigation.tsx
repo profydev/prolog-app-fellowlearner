bdc72618864c643ea4bab3285aca960ba731e6e8
ok <123243694+fellowlearner@users.noreply.github.com>
2023-12-11 17:30:36 -0800

    fixed-navigation-support-button to show user default email app with auto-filled recipient and subject when clicked

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -20,6 +20,20 @@ export function SidebarNavigation() {
   const router = useRouter();
   const { isSidebarCollapsed, toggleSidebar } = useContext(NavigationContext);
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const handleEmailClick = () => {
+    const email = "support@prolog-app.com";
+    const subject = "Support Request:";
+    //const body = 'How can we help?';
+
+    if (typeof window !== "undefined") {
+      const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
+        subject,
+      )}`;
+
+      // Open the user's default email client
+      window.location.href = mailtoLink;
+    }
+  };
   return (
     <div
       className={classNames(
@@ -83,7 +97,7 @@ export function SidebarNavigation() {
               text="Support"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              onClick={() => handleEmailClick()}
             />
             <MenuItemButton
               text="Collapse"
